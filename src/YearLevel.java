08a4013ec191cca47bbb470edde75f8c7322922e
Sam Matthew <sammatthew1418@gmail.com>
2026-05-27 17:59:05 +0800

    Update YearLevel.java

--- src/YearLevel.java
+++ src/YearLevel.java
@@ -1,1 +1,35 @@
+/*
+Name: Umandap, Sam Matthew B.
+Section: BSIT 2-2
+File Name: YearLevel.java
+*/
 
+public enum YearLevel {
+
+    FIRST_YEAR,
+    SECOND_YEAR,
+    THIRD_YEAR,
+    FOURTH_YEAR;
+
+    @Override
+    public String toString() {
+
+        switch (this) {
+
+            case FIRST_YEAR:
+                return "1st Year";
+
+            case SECOND_YEAR:
+                return "2nd Year";
+
+            case THIRD_YEAR:
+                return "3rd Year";
+
+            case FOURTH_YEAR:
+                return "4th Year";
+
+            default:
+                return "";
+        }
+    }
+}
